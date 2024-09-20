66bd7670ec53d576ddc51209c4005727550b13e0
Friedrich19 <santosofriedrichlitani@gmail.com>
2024-09-20 12:31:24 +0700

    finishing

--- app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -11,19 +11,12 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CafeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CafeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
