0b2f2dcdcc88663544d81e44092f3968a89e2517
Friedrich19 <santosofriedrichlitani@gmail.com>
2024-09-20 12:06:08 +0700

    finishing

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -5,13 +5,19 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
-    override fun getItemCount(): Int {
+val TABS_CONTENT_FIXED = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc,
+)
+
+class CafeAdapter(private val context: Context, fragmentManager: FragmentManager, lifecycle:
+
+    override fun getItemCount: Int {
         return TABS_FIXED.size
     }
     override fun createFragment(position: Int): Fragment
     {
-        return CafeDetailFragment()
+        return CafeDetailFragment.newInstance(context.getString(TABS_CONTENT_FIXED[position]
     }
-}
+)
